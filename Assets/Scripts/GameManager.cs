d1f714f25f9055d805de592dac91d7f0ed0fe8ee
taylorpitman <taylordianebusiness@gmail.com>
2024-03-18 20:00:06 -0400

    Finished Piggies + start of Win/Lose

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,19 +9,42 @@ public class GameManager : MonoBehaviour
     public static GameManager gameManager;
     public const int MAX_SHOTS = 3;
     private int currentNumShots = 0;
+
     [SerializeField] private IconHandler iconHandler;
+    [SerializeField] private float LevelEndDelay = 3f;
+
+    private List<Piggie> piggies= new List<Piggie>();
+
+    [SerializeField] private GameObject restartScreenObj;
+    [SerializeField] private SlingShotHandler slingShotHandler;
+
+
     private void Awake() 
     {
         if (gameManager == null)
         {
             gameManager = this;
         }
+
+        FindPiggies();
     
+    }
+
+    public void FindPiggies()
+    {
+        Piggie[] piggieList = FindObjectsOfType<Piggie>();
+
+        for (int i = 0; i < piggieList.Length; i++)
+        {
+            piggies.Add(piggieList[i]);
+        }
     }
     public void UseShot()
     {
         currentNumShots++;
         iconHandler.UseShot(currentNumShots);
+
+        CheckForLastShot();
     }
 
     public bool HasEnoughShots()
@@ -34,4 +58,59 @@ public class GameManager : MonoBehaviour
 
         return enoughShots;
     }
+
+    public void CheckForLastShot()
+    {
+        if(currentNumShots == MAX_SHOTS)
+        {
+            StartCoroutine(NoMoreShotsDelay());
+        }
+    }
+
+    public void CheckForLastDeadPiggie()
+    {
+        if(piggies.Count == 0)
+        {
+            WinGame();
+        }
+    }
+
+    private IEnumerator NoMoreShotsDelay()
+    {
+        yield return new WaitForSeconds(LevelEndDelay);
+
+        if(piggies.Count == 0)
+        {
+            WinGame();
+        }
+        else
+        {
+            LoseGame();
+        }
+    }
+
+    public void RemovePiggie(Piggie piggie)
+    {
+        piggies.Remove(piggie);
+        CheckForLastDeadPiggie();
+    }
+
+    #region Win/Lose
+
+    private void WinGame()
+    {
+        //makes restart screen visible
+        restartScreenObj.SetActive(true);
+
+    }
+
+    private void LoseGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    #endregion
+    
+
+    
 }
---
+++ Assets/Scripts/Piggie.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Piggie : MonoBehaviour
+{
+    [SerializeField] private const float MAX_HEALTH= 3f;
+    [SerializeField] private float damageThreshhold = 0.02f;
+    private float currentHealth;
+
+    private void Awake() 
+    {
+        currentHealth = MAX_HEALTH;
+    }
+    
+    public void DamagePiggie(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if(currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        GameManager.gameManager.RemovePiggie(this);
+        
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        float impactVelocity = collision.relativeVelocity.magnitude;
+
+        if(impactVelocity > damageThreshhold)
+        {
+            DamagePiggie(impactVelocity);
+        }
+    }
+}
